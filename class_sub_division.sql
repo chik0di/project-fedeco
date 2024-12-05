0f6c68410499df407eec5ed3144e871ea3f1bbcc
chik0di <chikodiobu@gmail.com>
2024-12-05 17:21:09 +0100

    Update DB Schema

--- class_sub_division.sql
+++ class_sub_division.sql
@@ -2,5 +2,6 @@
 (
 	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [class_id] INT NOT NULL, 
-    [division] NCHAR(10) NOT NULL
+    [division] NCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_class_sub_division_class] FOREIGN KEY ([class_id]) REFERENCES [class]([class_id])
 )
