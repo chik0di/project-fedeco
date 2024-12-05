a1616a0314fecf538a948fdb60827753f3fee729
chik0di <chikodiobu@gmail.com>
2024-12-05 15:27:03 +0100

    Update fedeco_db schema

--- class.sql
+++ class.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[class]
 (
-	[class_id] INT NOT NULL PRIMARY KEY, 
+	[class_id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [class] NCHAR(5) NOT NULL
 )
--- hostel.sql
+++ hostel.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[hostel]
 (
-	[id] INT NOT NULL PRIMARY KEY, 
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [name] VARCHAR(50) NOT NULL, 
     [slogan] NCHAR(10) NOT NULL
 )
