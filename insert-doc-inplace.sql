219d52451f2d7572148b404069354c01c32b5498
chik0di <chikodiobu@gmail.com>
2024-12-05 17:20:40 +0100

    Initial Commit Insert Into Document

---
+++ insert-doc-inplace.sql
@@ -0,0 +1,15 @@
+﻿-----------------------------------------------
+------INSERTING VALUES INTO THE TERM TABLE-----
+-----------------------------------------------
+INSERT INTO term (term)
+VALUES  ('1st'), 
+		('2nd'),
+		('3rd');
+
+
+-----------------------------------------------
+-----INSERTING VALUES INTO THE CLASS TABLE-----
+-----------------------------------------------
+INSERT INTO class (class)
+VALUES  ('JS1'), ('JS2'), ('JS3'),
+		('SS1'), ('SS2'), ('SS3');
