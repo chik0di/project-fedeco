a11c0d0b70348c4349808b8171815938cbdf3c07
chik0di <chikodiobu@gmail.com>
2024-12-07 19:28:36 +0100

    Update Schema

--- subject.sql
+++ subject.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[subject]
 (
-	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+	[subject_id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [name] VARCHAR(50) NOT NULL, 
     [subject_department_id] INT NOT NULL,
     CONSTRAINT [FK_subject_department_id] FOREIGN KEY ([subject_department_id]) REFERENCES [subject_department]([id])
