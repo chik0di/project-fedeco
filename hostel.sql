cb1d7c34b2e9245d5e5bd1b320fcff33e74ce792
chik0di <chikodiobu@gmail.com>
2024-12-07 17:52:05 +0100

    Update Schema

--- hostel.sql
+++ hostel.sql
@@ -2,5 +2,6 @@
 (
 	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [name] VARCHAR(50) NOT NULL, 
-    [slogan] NCHAR(10) NOT NULL
+    [hostel_master] VARCHAR(30) NOT NULL, 
+    CONSTRAINT [FK_hostel_staff] FOREIGN KEY ([hostel_master]) REFERENCES [staff]([staff_id])
 )
--- student.sql
+++ student.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[student]
 (
-	[id] VARCHAR(30) NOT NULL PRIMARY KEY, 
+	[student_id] VARCHAR(30) NOT NULL PRIMARY KEY, 
     [first_name] NCHAR(90) NOT NULL, 
     [middle_name] NCHAR(70) NOT NULL, 
     [last_name] NCHAR(90) NOT NULL, 
     [date_of_birth] DATE NOT NULL, 
     [date_of_enrolment] DATE NOT NULL, 
     [hostel_id] INT NOT NULL, 
-    [guardian] VARCHAR(30) NOT NULL, 
+    [guardian] VARCHAR(30) NULL, 
     CONSTRAINT [FK_student_staff] FOREIGN KEY ([guardian]) REFERENCES [staff]([staff_id]), 
     CONSTRAINT [FK_student_hostel] FOREIGN KEY ([hostel_id]) REFERENCES [hostel]([id])
 )
