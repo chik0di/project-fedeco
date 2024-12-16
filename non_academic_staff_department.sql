53cff8c9e7adb307678bb7c6bc420bd79a7bd2cb
chik0di <chikodiobu@gmail.com>
2024-12-16 16:31:26 +0100

    Add UNIQUE Constraint to the Candidate Keys

--- non_academic_staff_department.sql
+++ non_academic_staff_department.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[non_academic_staff_department]
 (
 	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [name] VARCHAR(50) NOT NULL
+    [name] VARCHAR(50) UNIQUE NOT NULL
 )
--- non_teaching_staff_role.sql
+++ non_teaching_staff_role.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[non_teaching_staff_role]
 (
 	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [name] VARCHAR(50) NOT NULL
+    [name] VARCHAR(50) UNIQUE NOT NULL
 )
