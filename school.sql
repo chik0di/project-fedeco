21d4bae9edbf5116c925809f0df118ffae579628
chik0di <chikodiobu@gmail.com>
2024-12-19 02:29:28 +0100

    Update Schema

--- school.sql
+++ school.sql
@@ -5,11 +5,10 @@
     [school_code] VARCHAR(50) NOT NULL UNIQUE, 
     [address] NCHAR(121) NOT NULL,
     [zipcode] INT NOT NULL,
-    [local_government_area_id] INT NOT NULL,
-    [state_id] INT NOT NULL,
+    [state] NCHAR(50) NOT NULL,
     [school_website] NCHAR(196) NULL UNIQUE,
     [email_address] NCHAR(127) NOT NULL UNIQUE,
     [year_established] INT NOT NULL,
-    CONSTRAINT [FK_school_state] FOREIGN KEY ([state_id]) REFERENCES [state]([id]), 
+    CONSTRAINT [FK_school_state] FOREIGN KEY ([state]) REFERENCES [state]([state]), 
     CONSTRAINT [CK_school_years_valid] CHECK (year_established >= 1963 AND year_established <= YEAR(GETDATE()))
 )
