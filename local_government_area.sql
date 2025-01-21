f91b01f70a2dffa5ec605c6abaf38796552f6147
chik0di <chikodiobu@gmail.com>
2025-01-21 22:49:56 +0100

    Update Schema - lga

--- local_government_area.sql
+++ local_government_area.sql
@@ -2,6 +2,6 @@
 (
 	[id] INT NOT NULL PRIMARY KEY, 
     [local_government] NCHAR(90) NOT NULL, 
-    [state] NCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_local_government_area_state] FOREIGN KEY ([state]) REFERENCES [state]([state])
+    [state_id] INT NOT NULL
+    CONSTRAINT [FK_local_government_area_state] FOREIGN KEY ([state_id]) REFERENCES [state]([id])
 )
