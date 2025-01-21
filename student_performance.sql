d216147f392ab3395322206b28df2a8e11450586
chik0di <chikodiobu@gmail.com>
2025-01-21 22:48:25 +0100

    Update Schema

--- student_performance.sql
+++ student_performance.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[student_performance]
 (
-	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+	[id] INT NOT NULL, 
     [student_id] VARCHAR(30) NOT NULL, 
     [class_division_id] INT NOT NULL, 
     [term_id] INT NOT NULL, 
     [subject_id] INT NOT NULL, 
     [first_ca] INT NOT NULL, 
     [second_ca] INT NOT NULL, 
-    [exam] INT NOT NULL, 
+    [exam] INT NOT NULL
     CONSTRAINT [CK_student_performance_first_ca] CHECK (first_ca BETWEEN 0 AND 30), 
     CONSTRAINT [CK_student_performance_second_ca] CHECK (second_ca BETWEEN 0 AND 30), 
     CONSTRAINT [CK_student_performance_exam] CHECK (exam BETWEEN 0 AND 70), 
