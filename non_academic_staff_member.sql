5145613bab2b8bcd0600d76e1429666459075e18
Chikodi <128258883+chik0di@users.noreply.github.com>
2025-02-05 17:22:33 +0100

    Delete non_academic_staff_member.sql

--- non_academic_staff_member.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[non_academic_staff_member] (
-    [id]                               INT          NOT NULL,
-    [staff_id]                         VARCHAR (50) NOT NULL,
-    [non_academic_staff_department_id] INT          NOT NULL,
-    [date_started]                     DATE         NOT NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC),
-    CONSTRAINT [FK_non_academic_staff_department_id] FOREIGN KEY ([non_academic_staff_department_id]) REFERENCES [dbo].[non_academic_staff_department] ([id])
-);
