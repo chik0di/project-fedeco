fc9d7a0b47b21e1518cd25af0a0391437096eb6e
Chikodi <128258883+chik0di@users.noreply.github.com>
2025-02-05 17:41:19 +0100

    Delete teaching_staff.sql

--- teaching_staff.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[teaching_staff]
-(
-	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [staff_id] VARCHAR(30) NOT NULL, 
-    [subject_id] INT NOT NULL, 
-    CONSTRAINT [FK_subject_id] FOREIGN KEY ([subject_id]) REFERENCES [subject]([subject_id])    
-)
