69f2995ab64346359d0ee491544106716290aa0e
chik0di <chikodiobu@gmail.com>
2025-02-12 17:15:56 +0100

    Create Dimension tables

---
+++ DimAcademicYear.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[DimAcademicYear]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [session] NCHAR(10) UNIQUE NOT NULL
+)
---
+++ DimClass.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[DimClass]
+(
+	[class_id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [class] NCHAR(5) UNIQUE NOT NULL
+)
