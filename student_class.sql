daed6d5e9ca0623a629214dc4b36dbe6bbc14561
chik0di <chikodiobu@gmail.com>
2025-02-15 17:11:54 +0100

    Update entities

--- student_class.sql
+++ student_class.sql
@@ -3,5 +3,5 @@
 	[student_id] VARCHAR(30) NOT NULL PRIMARY KEY, 
     [class_id] INT NOT NULL, 
     CONSTRAINT [FK_student_class_student] FOREIGN KEY ([student_id]) REFERENCES [student]([student_id]),
-    CONSTRAINT [FK_student_class_class] FOREIGN KEY ([class_id]) REFERENCES [class]([class_id])
+    CONSTRAINT [FK_student_class_class] FOREIGN KEY ([class_id]) REFERENCES [DimClass]([class_id])
 )
--- teaching_staff.sql
+++ teaching_staff.sql
@@ -3,5 +3,5 @@
 	[id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [staff_id] VARCHAR(30) NOT NULL, 
     [subject_id] INT NOT NULL, 
-    CONSTRAINT [FK_subject_id] FOREIGN KEY ([subject_id]) REFERENCES [subject]([subject_id])    
+    CONSTRAINT [FK_subject_id] FOREIGN KEY ([subject_id]) REFERENCES [DimSubject]([subject_id])    
 )
