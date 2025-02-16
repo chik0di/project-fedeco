e6765a6540e3a4988398adcd0bd7f9b46a3f638a
chik0di <chikodiobu@gmail.com>
2025-02-16 22:00:19 +0100

    Update Schema

--- DimStudent.sql
+++ DimStudent.sql
@@ -5,7 +5,7 @@
     [middle_name] NCHAR(70) NOT NULL, 
     [last_name] NCHAR(90) NOT NULL, 
     [date_of_birth] DATE NOT NULL, 
-    [academic_year_of_admission] NCHAR(10) NOT NULL,
-    [state_of_origin] NCHAR(90) NULL, 
-    CONSTRAINT [FK_student_academic_year] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [DimAcademicYear]([session])
+    [state_of_origin] INT NULL
+    CONSTRAINT [FK_DimStudent_DimState] FOREIGN KEY (state_of_origin) REFERENCES [state]([id])
+
 )
