e88b153cf66561e04189538f997dcea594c12af0
chik0di <chikodiobu@gmail.com>
2025-02-16 21:57:11 +0100

    Delete entity

--- student_class.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[student_class]
-(
-	[student_id] VARCHAR(30) NOT NULL PRIMARY KEY, 
-    [class_id] INT NOT NULL, 
-    CONSTRAINT [FK_student_class_student] FOREIGN KEY ([student_id]) REFERENCES [student]([student_id]),
-    CONSTRAINT [FK_student_class_class] FOREIGN KEY ([class_id]) REFERENCES [DimClass]([class_id])
-)
