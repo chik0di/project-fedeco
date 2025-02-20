4e9fa7632bcc1fe107eb7f62b218d63151b496fa
chik0di <chikodiobu@gmail.com>
2025-02-20 17:44:33 +0100

    Update Fact Table

--- StaffSchoolFact.sql
+++ StaffSchoolFact.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[StaffSchoolFact]
 (
-	[staff_id] INT NOT NULL PRIMARY KEY, 
+	[staff_id] INT NOT NULL, 
     [school_id] NCHAR(10) NOT NULL,
-	[date_started] DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+	[employment_type_id] INT NOT NULL,
+	[date_started] DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     [date_left] DATE NULL
 )
