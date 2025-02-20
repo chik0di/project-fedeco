9e298ed330eceea8cb40c8e41db80a89f0431ba6
chik0di <chikodiobu@gmail.com>
2025-02-20 17:44:11 +0100

    Create New Dimension Tables

---
+++ EmploymentTypeDim.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[EmploymentTypeDim]
+(
+	[id] INT NOT NULL PRIMARY KEY,
+	[employment_type] VARCHAR(50) NOT NULL
+)
---
+++ NonTeachingStaffDim.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[NonTeachingStaffDim]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [name] VARCHAR(50) UNIQUE NOT NULL
+)
