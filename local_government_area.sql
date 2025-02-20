bea9af1acd06401baa0c33eb50ca29ea8f199768
chik0di <chikodiobu@gmail.com>
2025-02-20 23:11:46 +0100

    Delete Entities

--- local_government_area.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[local_government_area]
-(
-	[id] INT NOT NULL PRIMARY KEY, 
-    [local_government] NCHAR(90) NOT NULL, 
-    [state_id] INT NOT NULL
-    CONSTRAINT [FK_local_government_area_state] FOREIGN KEY ([state_id]) REFERENCES [state]([id])
-)
