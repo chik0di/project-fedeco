451c70a869d2620e08e238519d53c456b4ef1adf
chik0di <chikodiobu@gmail.com>
2025-03-01 18:14:40 +0100

    Update Table

--- StaffSchoolFact.sql
+++ StaffSchoolFact.sql
@@ -3,6 +3,8 @@
 	[staff_id] INT NOT NULL, 
     [school_id] NCHAR(10) NOT NULL,
 	[employment_type_id] INT NOT NULL,
+	[subject_id] INT NULL,
+	[non_teaching_role_id] INT NULL,
 	[date_started] DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     [date_left] DATE NULL
 )
