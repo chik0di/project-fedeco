f4732ae868807e26a9c60bb016f349efebc12025
chik0di <chikodiobu@gmail.com>
2025-03-01 18:53:18 +0100

    Update Table

--- PayrollFact.sql
+++ PayrollFact.sql
@@ -3,5 +3,5 @@
 	[staff_id] INT NOT NULL, 
     [school_id] NCHAR(10) NOT NULL,
 	[level] INT,
-	[salary] FLOAT
+	[salary] FLOAT NOT NULL
 )
--- StaffSchoolFact.sql
+++ StaffSchoolFact.sql
@@ -6,5 +6,7 @@
 	[subject_id] INT NULL,
 	[non_teaching_role_id] INT NULL,
 	[date_started] DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    [date_left] DATE NULL
+    [until_date] DATE NULL,
+	CONSTRAINT [FK_subject_id] FOREIGN KEY ([subject_id]) REFERENCES [SubjectDim]([subject_id]),
+	CONSTRAINT [FK_non_teaching_role_id] FOREIGN KEY ([non_teaching_role_id]) REFERENCES [NonTeachingDim]([id])
 )
