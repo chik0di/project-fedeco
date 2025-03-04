9f6bb469ba834b564c688d9c8486bb5635f38e6e
chik0di <chikodiobu@gmail.com>
2025-03-04 14:04:10 +0100

    Update StudentSchoolFact

--- StudentSchoolFact.sql
+++ StudentSchoolFact.sql
@@ -6,7 +6,7 @@
     [academic_year_finished] INT NULL,
     [class_admitted] INT NOT NULL,
     [student_type] VARCHAR(15) NOT NULL
-    CONSTRAINT [FK_DimStudentSchool_DimAcademic_year_start] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [DimAcademicYear]([id]),
-    CONSTRAINT [FK_DimStudentSchool_DimAcademic_year_finish] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [DimAcademicYear]([id]),
-    CONSTRAINT [FK_DimStudentSchool_DimClass] FOREIGN KEY (class_admitted) REFERENCES [DimClass]([class_id])
+    CONSTRAINT [FK_DimStudentSchool_DimAcademic_year_start] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [AcademicYearDim]([id]),
+    CONSTRAINT [FK_DimStudentSchool_DimAcademic_year_finish] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [AcademicYearDim]([id]),
+    CONSTRAINT [FK_DimStudentSchool_DimClass] FOREIGN KEY (class_admitted) REFERENCES [ClassDim]([class_id])
 )
