172e9b665f4cce77b0393502fc679b8ef273bb55
chik0di <chikodiobu@gmail.com>
2025-03-04 14:04:36 +0100

    Update Student Performance

--- StudentPerformanceFact.sql
+++ StudentPerformanceFact.sql
@@ -8,11 +8,11 @@
     [first_ca] INT NOT NULL, 
     [second_ca] INT NOT NULL, 
     [exam] INT NOT NULL
-    CONSTRAINT [CK_student_performance_first_ca] CHECK (first_ca BETWEEN 0 AND 30), 
-    CONSTRAINT [CK_student_performance_second_ca] CHECK (second_ca BETWEEN 0 AND 30), 
-    CONSTRAINT [CK_student_performance_exam] CHECK (exam BETWEEN 0 AND 70), 
-    CONSTRAINT [FK_student_performance_student] FOREIGN KEY ([student_id]) REFERENCES [DimStudent]([student_id]), 
-    CONSTRAINT [FK_student_performance_term] FOREIGN KEY ([term_id]) REFERENCES [DimTerm]([term_id]), 
-    CONSTRAINT [FK_student_performance_class] FOREIGN KEY ([class_id]) REFERENCES [DimClass]([class_id]), 
-    CONSTRAINT [FK_student_performance_subject] FOREIGN KEY ([subject_id]) REFERENCES [DimSubject]([subject_id])
+    -- CONSTRAINT [CK_student_performance_first_ca] CHECK (first_ca BETWEEN 0 AND 30), 
+    -- CONSTRAINT [CK_student_performance_second_ca] CHECK (second_ca BETWEEN 0 AND 30), 
+    -- CONSTRAINT [CK_student_performance_exam] CHECK (exam BETWEEN 0 AND 70), 
+    CONSTRAINT [FK_student_performance_student] FOREIGN KEY ([student_id]) REFERENCES [StudentDim]([student_id]), 
+    CONSTRAINT [FK_student_performance_term] FOREIGN KEY ([term_id]) REFERENCES [TermDim]([term_id]), 
+    CONSTRAINT [FK_student_performance_class] FOREIGN KEY ([class_id]) REFERENCES [ClassDim]([class_id]), 
+    CONSTRAINT [FK_student_performance_subject] FOREIGN KEY ([subject_id]) REFERENCES [SubjectDim]([subject_id])
     )
