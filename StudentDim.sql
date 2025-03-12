f2b56fee02f36faf609fde8c8d359846d165c0d6
chik0di <chikodiobu@gmail.com>
2025-03-12 13:45:02 +0100

    Updated StudentDim: Added UUID for each student

--- StudentDim.sql
+++ StudentDim.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[StudentDim]
-(
-	[student_id] VARCHAR(30) NOT NULL PRIMARY KEY, 
+(   
+    [record_id] INT PRIMARY KEY IDENTITY(1,1),
+	[student_id] UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL UNIQUE, 
     [first_name] NCHAR(90) NOT NULL, 
     [middle_name] NCHAR(70) NOT NULL, 
     [last_name] NCHAR(90) NOT NULL, 
-    [date_of_birth] DATE NOT NULL, 
+    [date_of_birth] DATE NOT NULL,
+    [gender] NCHAR(1) NOT NULL,
     [state_of_origin] INT NULL
     CONSTRAINT [FK_DimStudent_DimState] FOREIGN KEY (state_of_origin) REFERENCES [StateDim]([id])
 
