68fcaf03b2b7c841d6db69c1ca77042afee42278
chik0di <chikodiobu@gmail.com>
2025-03-12 14:00:04 +0100

    Update StaffDim

--- StaffDim.sql
+++ StaffDim.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[StaffDim]
 (
 	[record_id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [staff_id] VARCHAR(45) NOT NULL UNIQUE,
+    [staff_id] UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL UNIQUE,
     [first_name] VARCHAR(50) NOT NULL, 
     [middle_name] VARCHAR(50) NOT NULL,
     [last_name] VARCHAR(50) NOT NULL, 
     [date_of_birth] DATE NOT NULL, 
     [gender] NCHAR(1) NOT NULL, 
+    [state_id] INT NULL,
     [phone_number] INT NOT NULL, 
     [email_address] VARCHAR(70) UNIQUE NOT NULL
+    CONSTRAINT [FK_StaffDim_DimState] FOREIGN KEY (state_id) REFERENCES [StateDim]([id])
 )
