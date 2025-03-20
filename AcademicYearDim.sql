5025c5d1ce4f4ca114ae045b543c93bc7645cff6
chik0di <chikodiobu@gmail.com>
2025-03-20 03:22:11 +0100

    Update Schema: Add Auto Increment to primary key

--- AcademicYearDim.sql
+++ AcademicYearDim.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[AcademicYearDim]
 (
-	[id] INT NOT NULL PRIMARY KEY, 
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
     [session] NCHAR(10) UNIQUE NOT NULL, 
     [start_date] DATE NOT NULL, 
     [end_date] DATE NOT NULL
