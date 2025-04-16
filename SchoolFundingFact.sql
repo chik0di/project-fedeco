778ac4f82312b35f020df1111e139ff9088035e6
chik0di <chikodiobu@gmail.com>
2025-04-16 18:24:44 +0100

    Initial Commit

---
+++ SchoolFundingFact.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SchoolFundingFact]
+(
+	[school_funding_id] INT NOT NULL PRIMARY KEY, 
+    [school_id] NCHAR(10) NOT NULL, 
+    [funding_source] NCHAR(90) NOT NULL, 
+    [amount_allocated] DECIMAL NOT NULL, 
+    [date_allocated] DATE NULL
+)
---
+++ StaffSchoolFact.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[StaffSchoolFact]
+(
+	[staff_id] INT NOT NULL, 
+    [school_id] NCHAR(10) NOT NULL,
+	[employment_type_id] INT NOT NULL,
+	[subject_id] INT NULL,
+	[non_teaching_role_id] INT NULL,
+	[hire_date] DATE NOT NULL,
+    [until_date] DATE NULL,
+	[created_at] DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+	CONSTRAINT [FK_subject_id] FOREIGN KEY ([subject_id]) REFERENCES [SubjectDim]([subject_id]),
+	CONSTRAINT [FK_non_teaching_role_id] FOREIGN KEY ([non_teaching_role_id]) REFERENCES [NonTeachingDim]([id])
+)
