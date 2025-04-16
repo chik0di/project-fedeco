80b850b99c212a5affd2bc08f0d1f383e4f6c8de
chik0di <chikodiobu@gmail.com>
2025-04-16 18:01:08 +0100

    Delete dbo Folder & move content to root solution

--- dbo/Tables/FundingExpenseJunctionFact.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[FundingExpenseJunctionFact]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [school_funding_id] INT NOT NULL,
-	[expense_category_id] INT NOT NULL, 
-    [amount_spent] DECIMAL NOT NULL, 
-)
--- dbo/Tables/SubjectDim.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[SubjectDim]
-(
-	[subject_id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [name] VARCHAR(50) NOT NULL, 
-    [subject_department_id] INT NOT NULL,
-    CONSTRAINT [FK_subject_department_id] FOREIGN KEY ([subject_department_id]) REFERENCES [SubjectDepartmentDim]([id])
-)
