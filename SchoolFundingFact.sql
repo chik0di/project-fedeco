65ee94be1a8bd024486c1279162370642c7345fb
Chikodi <128258883+chik0di@users.noreply.github.com>
2025-05-05 15:07:57 +0100

    Delete SchoolFundingFact.sql

--- SchoolFundingFact.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[SchoolFundingFact]
-(
-	[school_funding_id] INT NOT NULL PRIMARY KEY, 
-    [school_id] NCHAR(10) NOT NULL, 
-    [funding_title] NCHAR(90) NOT NULL,
-    [funding_type] NCHAR(20) NOT NULL CHECK(funding_type IN('Government Funding', '')),
-    [amount_allocated] DECIMAL NOT NULL, 
-    [date_allocated] DATE NULL
-)
