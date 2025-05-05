cb8c24fbc150825612f619c3a58be5b35f1d5823
Chikodi <128258883+chik0di@users.noreply.github.com>
2025-05-05 15:07:06 +0100

    Delete AcademicYearDim.sql

--- AcademicYearDim.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[AcademicYearDim]
-(
-	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [session] NCHAR(10) UNIQUE NOT NULL, 
-    [start_date] DATE NOT NULL, 
-    [end_date] DATE NOT NULL
-)
