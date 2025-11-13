b8e0a8a8570fc9e3edadac8a644a1613b5c786ab
chik0di <chikodiobu@gmail.com>
2025-11-13 15:14:01 +0100

    Update StudentSchoolFact.sql

--- db-objects/StudentSchoolFact.sql
+++ db-objects/StudentSchoolFact.sql
@@ -6,11 +6,10 @@
     [enrolment_date] DATE NOT NULL,
 	[academic_year_of_admission] INT NOT NULL,
     [status] NCHAR(14) NOT NULL CHECK(status IN('Active', 'Graduated', 'Transferred', 'Dropped')) DEFAULT 'Active',
-    [academic_year_finished] INT NULL,
     [class_admitted] INT NOT NULL,
-    [current_class] INT NOT NULL,
-    [student_type] VARCHAR(6) NOT NULL CHECK(student_type IN('Day', 'Boarder'))
+    [student_type] VARCHAR(6) NOT NULL CHECK(student_type IN('Day', 'Boarder')),
+    [created_at] DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+    [updated_at] DATETIME NULL
     CONSTRAINT [FK_DimStudentSchool_DimAcademic_year_start] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [AcademicYearDim]([id]),
-    CONSTRAINT [FK_DimStudentSchool_DimAcademic_year_finish] FOREIGN KEY ([academic_year_of_admission]) REFERENCES [AcademicYearDim]([id]),
     CONSTRAINT [FK_DimStudentSchool_DimClass] FOREIGN KEY (class_admitted) REFERENCES [ClassDim]([class_id])
 )
