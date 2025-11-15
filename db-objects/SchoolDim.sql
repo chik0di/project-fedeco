57b9cfa776332bafc5708239f57420d56328197e
chik0di <chikodiobu@gmail.com>
2025-11-15 21:54:03 +0100

    Update table object

--- db-objects/SchoolDim.sql
+++ db-objects/SchoolDim.sql
@@ -4,7 +4,5 @@
     [full_name] NCHAR(131) NOT NULL, 
     [school_display] VARCHAR(50) NOT NULL, 
     [state] NCHAR(50) NOT NULL,
-    [school_website] NCHAR(196) NULL, 
-    CONSTRAINT [FK_SchoolDim_State] FOREIGN KEY ([state]) REFERENCES [StateDim]([state])
-
+    [school_website] NCHAR(196) NULL
 )
